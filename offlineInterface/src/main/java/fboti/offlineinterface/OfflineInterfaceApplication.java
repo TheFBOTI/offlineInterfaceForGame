4d4e048bea3ddfef46341f68eef36c8f3ee9877c
TheFBOTI <martyn.b.se@gmail.com>
2026-05-20 11:48:14 +0100

    initial commit for project using internal spring builder tooling

---
+++ offlineInterface/src/main/java/fboti/offlineinterface/OfflineInterfaceApplication.java
@@ -0,0 +1,13 @@
+package fboti.offlineinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflineInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfflineInterfaceApplication.class, args);
+	}
+
+}
---
+++ offlineInterface/src/test/java/fboti/offlineinterface/OfflineInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package fboti.offlineinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflineInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
